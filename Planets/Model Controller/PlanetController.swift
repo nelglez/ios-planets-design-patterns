5171a5d6233845037dbd745cd92532c83356518b
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-07 12:39:39 -0500

    implement facade pattern

--- Planets/Model Controller/PlanetController.swift
+++ Planets/Model Controller/PlanetController.swift
@@ -9,26 +9,27 @@
 import Foundation
 
 class PlanetController {
-    var planetsWithoutPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        ]
+    //Facade pattern
+    //Computer property takes care of figuring out whether pluto should be included or not. This way, nothing else in the app has to figure it out.
+    var planets: [Planet] {
+        var planets =  [
+            Planet(name: "Mercury", imageName: "mercury"),
+            Planet(name: "Venus", imageName: "venus"),
+            Planet(name: "Earth", imageName: "earth"),
+            Planet(name: "Mars", imageName: "mars"),
+            Planet(name: "Jupiter", imageName: "jupiter"),
+            Planet(name: "Saturn", imageName: "saturn"),
+            Planet(name: "Uranus", imageName: "uranus"),
+            Planet(name: "Neptune", imageName: "neptune"),
+            ]
+        
+        let shouldShowPluto = UserDefaults.standard.bool(forKey: .shouldShowPlutoKey)
+        
+        if shouldShowPluto {
+            planets.append(Planet(name: "Pluto", imageName: "pluto"))
+        }
+        
+        return planets
+    }
     
-    var planetsWithPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        Planet(name: "Pluto", imageName: "pluto"),
-        ]
 }
